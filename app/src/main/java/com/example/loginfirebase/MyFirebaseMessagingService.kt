c446cb09d0f684400ab7c7be854d3e60e6e5ba2a
MadHapper <kevinbermudez999z@gmail.com>
2024-02-09 22:36:18 -0500

    firebase messeaging MessageReceived

--- app/src/main/java/com/example/loginfirebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/loginfirebase/MyFirebaseMessagingService.kt
@@ -1,6 +1,11 @@
 package com.example.loginfirebase
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
 }
